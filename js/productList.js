0b40a4f419a145b3126499c3c1b1b8f9dc92d7c4
JunSean Fung <jf333@njit.edu>
2021-02-21 13:48:12 -0500

    small commit

--- js/productList.js
+++ js/productList.js
@@ -10,7 +10,32 @@
 (function () {
 
 
+    var databaseOne = [
+        {_id: '123', name: 'Article 1', published: true},
+        {_id: '583', name: 'Article 2', published: true},
+        {_id: '954', name: 'Article 3', published: false},
+        {_id: '384', name: 'Article 4', published: false},
+        {_id: '183', name: 'Article 5', published: true},
+        {_id: '007', name: 'Article 6', published: false},
+        {_id: '304', name: 'Article 7', published: true},
 
+        function renderList (results) {
+            // Purpose: render the list
+            var tableBody = document.querySelector('#results-table tbody');
+
+            tableBody.innerHTML = ' '; //Clear the innerHTML
+
+            var tableRows = results.map(function (result, index ){
+                return '<tr><td>' + index + '</td><td>' + result.name + '</td><td>' +
+                    result._id + '</td><td>' + result.published + '</td></tr>';
+            });
+
+            // Set the contents of the table body to the new set of rendered HTML rows
+            tableRows.forEach(function (row){
+                tableBody.innerHTML += row;
+            });
+        }
+    ]
     /*
     // Prof Example
     var databaseOne = [
@@ -39,14 +64,7 @@
             tableBody.innerHTML += row;
         });
     }
-    function sortItems (priceValue) {
-        /!* Purpose:
-            Sort items based on:
-        *                   1) Price
-        *                   2) Title
-*       *!/
 
-    }
     renderList(databaseOne); // Function call*/
 
 })(); // End of closure
