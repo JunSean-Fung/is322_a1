2a62d93275cbd6cda1b49d1c68dc787eb9667e25
JunSean Fung <jf333@njit.edu>
2021-02-21 16:49:22 -0500

    small commit

--- js/productList.js
+++ js/productList.js
@@ -8,64 +8,17 @@
 
 // Closure
 (function () {
+    $('#search').keyup(function (){
+        $('.card').removeClass('d-none');
+        var filter = $(this).val(); // get the value of the input, which we filter on
+        $('.card-deck').find('.card .card-body h4:not(:contains("'+filter+'"))').parent().parent().addClass('d-none');
+    })
 
-
-    var databaseOne = [
-        {_id: '123', name: 'Article 1', published: true},
-        {_id: '583', name: 'Article 2', published: true},
-        {_id: '954', name: 'Article 3', published: false},
-        {_id: '384', name: 'Article 4', published: false},
-        {_id: '183', name: 'Article 5', published: true},
-        {_id: '007', name: 'Article 6', published: false},
-        {_id: '304', name: 'Article 7', published: true},
-
-        function renderList (results) {
-            // Purpose: render the list
-            var tableBody = document.querySelector('#results-table tbody');
-
-            tableBody.innerHTML = ' '; //Clear the innerHTML
-
-            var tableRows = results.map(function (result, index ){
-                return '<tr><td>' + index + '</td><td>' + result.name + '</td><td>' +
-                    result._id + '</td><td>' + result.published + '</td></tr>';
-            });
-
-            // Set the contents of the table body to the new set of rendered HTML rows
-            tableRows.forEach(function (row){
-                tableBody.innerHTML += row;
-            });
-        }
-    ]
-    /*
-    // Prof Example
-    var databaseOne = [
-        {_id: '123', name: 'Article 1', published: true},
-        {_id: '583', name: 'Article 2', published: true},
-        {_id: '954', name: 'Article 3', published: false},
-        {_id: '384', name: 'Article 4', published: false},
-        {_id: '183', name: 'Article 5', published: true},
-        {_id: '007', name: 'Article 6', published: false},
-        {_id: '304', name: 'Article 7', published: true},
-    ]
-
-    function renderList (results) {
-        // Purpose: render the list
-        var tableBody = document.querySelector('#results-table tbody');
-
-        tableBody.innerHTML = ' '; //Clear the innerHTML
-
-        var tableRows = results.map(function (result, index ){
-            return '<tr><td>' + index + '</td><td>' + result.name + '</td><td>' +
-                result._id + '</td><td>' + result.published + '</td></tr>';
-        });
-
-        // Set the contents of the table body to the new set of rendered HTML rows
-        tableRows.forEach(function (row){
-            tableBody.innerHTML += row;
-        });
-    }
-
-    renderList(databaseOne); // Function call*/
+    $('#btnSort').click(function (){
+        $('.card-deck .card').sort(function(a,b) {
+            return $(a).find(".card-title").text() > $(b).find(".card-title").text() ? 1 : -1;
+        }).appendTo(".card-deck");
+    })
 
 })(); // End of closure
 
